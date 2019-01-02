3fa17ca12a89788366027d5ef6d6d5fdf428acee
Katrine <kst@es.aau.dk>
2019-01-02 16:29:32 +0100

    Initial commit

---
+++ mainParticipant+Dealer.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 10 15:59:42 2018
+
+@author: kst
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 30 11:31:50 2018
+
+@author: kst
+"""
+
+import socket
+import numpy as np
+from threading import Thread
+import FFArithmetic as field
+import shamir_scheme as ss
+import proc
+import TcpSocket5 as sock
+import time
+import queue as que
+from participantCode import party
+
+party_addr = [['192.168.100.10', 62], #P0
+              ['192.168.100.30', 62], #P1
+              ['192.168.100.31', 62], #P2
+              ['192.168.100.41', 62] #P3
+              #['192.168.100.40', 62], #Receiver 4
+              #['192.168.100.20', 62] #Reciever 5
+              ]
+
+class commsThread (Thread):
+   stop = False  
+   def __init__(self, threadID, name, server_info,q):
+      Thread.__init__(self)
+      self.q = q
+      self.threadID = threadID
+      self.name = name
+      self.server_info = server_info  # (Tcp_ip, Tcp_port)
+      self.Rx_packet = [] # tuple [[client_ip, client_port], [Rx_data[n]]]
+
+   def run(self):
+#      print("Starting " + self.name)      
+      #Create TCP socket
+      tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+      tcpsock.bind(tuple(self.server_info))
+      #Communication loop - Wait->Receive->Put to queue
+      while not self.stop:
+         Rx_packet = sock.TCPserver(tcpsock)
+#         print("Client info:",Rx_packet[0])
+#         print("Data recv:",Rx_packet[1])
+         if not self.q.full():
+            self.q.put(Rx_packet)
+      print("Exiting " + self.name)
+
+class UDPcommsThread(Thread):
+   def __init__(self, threadID, name, server_info):
+      Thread.__init__(self)
+      self.threadID = threadID
+      self.name = name
+      self.server_info = server_info  # (Tcp_ip, Tcp_port)
+      self.Rx_packet = []  # tuple [[client_ip, client_port], [Rx_data[n]]]
+
+   def run(self):
+      print("Starting " + self.name)
+      #Create UDP socket
+      udpsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      udpsock.bind((self.server_info[0], self.server_info[1]))
+      print('UDP Server Started:', self.server_info[0], self.server_info[1])
+
+      #Communication loop - Wait->Receive->Put to queue
+      while True:
+         Rx_data = sock.UDPserver(udpsock)
+         if not q.full():
+            q2.put(Rx_data)
+
+      udpsock.close()
+      print("Exiting " + self.name)
+
+class dealer():
+    def __init__(self,F, n, t, numTrip):
+        self.n = n
+        b = ss.share(F,np.random.choice([-1,1]), t, n)
+        self.distribute_shares('b', b)
+        triplets = [proc.triplet(F,n,t) for i in range(numTrip)]
+        for i in range(n):
+            l = []
+            for j in range(numTrip):
+                l.append(triplets[j][i])
+            sock.TCPclient(party_addr[i][0], party_addr[i][1], ['triplets' , l])
+        
+    def distribute_shares(self, name, s):
+        for i in range(self.n):
+            sock.TCPclient(party_addr[i][0], party_addr[i][1], [name , int(str(s[i]))])
+
+
+m = 7979490791
+mm = 97
+F = field.GF(m)            
+n = 4
+t = 1
+x = 5 #np.random.randint(0,50,40)
+pnr = party_addr.index([socket.gethostbyname(socket.gethostname()), 62])
+
+q = que.Queue()
+q2 = que.LifoQueue()
+
+
+#Initialization..
+#TCP_IP = '192.168.100.246'
+#TCP_PORT = 62
+UDP_PORT2 = 3000
+server_info = party_addr[pnr] #(TCP_IP, TCP_PORT)
+server2_info = (server_info[0], UDP_PORT2)
+
+# Create new threads..
+t1_comms = commsThread(1, "Communication Thread", server_info,q)
+#t2_commsSimulink = UDPcommsThread(2, "t2_commsSimulink", server2_info)
+p0 = party(F,int(x),n,t, pnr, q, q2)
+
+# Start new Threads
+#t2_commsSimulink.start()
+t1_comms.start()
+
+for i in party_addr:
+    while True:
+        try:
+            sock.TCPclient(i[0], i[1], ['flag', 1])
+            break
+        except:
+            continue
+
+deal = dealer(F,n,t,50)
+p0.start()
+
+
+
+
+
+
+
---
+++ participantCodePLOT.py
@@ -0,0 +1,177 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  3 11:10:12 2018
+
+@author: kst
+"""
+
+
+import numpy as np
+from threading import Thread
+import shamir_scheme as ss
+import proc
+import TcpSocket5 as sock
+import queue as que
+import time
+
+dd = np.random.randint(50, size=500)
+
+party_addr = [['192.168.100.10', 62], #P0
+              ['192.168.100.30', 62], #P1
+              ['192.168.100.31', 62], #P2
+              ['192.168.100.41', 62] #P3
+              #['192.168.100.40', 62], #Receiver 4
+              #['192.168.100.20', 62] #Reciever 5
+              ]
+
+ccu_adr = '192.168.100.245'
+
+server_addr = [[ccu_adr, 4010], #P0
+               [ccu_adr, 4011], #P1
+               [ccu_adr, 4030], #P2
+               [ccu_adr, 4031],               #P3
+               [ccu_adr, 4040],               #Reciever 4
+               [ccu_adr, 4041]                #Reciever 5
+              ]
+
+data = [1,10,15,13,22]
+
+class party(Thread):
+    def __init__(self, F, x, n, t, i, q, q2,q3):
+        Thread.__init__(self)
+        self.c = 0
+        self.comr = 0
+        self.recv = {}
+        self.F = F
+        self.x = x
+        self.n = n
+        self.t = t
+        self.i = i
+        self.q = q
+        self.q2 = q2
+        self.q3 = q3
+        
+    def distribute_shares(self, sec):
+        shares = ss.share(self.F, sec, self.t, self.n)
+        for i in range(self.n):
+            sock.TCPclient(party_addr[i][0], party_addr[i][1], ['input' + str(self.i) , int(str(shares[i]))])
+        
+    def broadcast(self, name, s):
+        for i in range(self.n):
+            sock.TCPclient(party_addr[i][0], party_addr[i][1], [name + str(self.i) , int(str(s))])
+                    
+    def readQueue(self):
+        while not self.q.empty():
+            b = self.q.get()[1]
+            self.recv[b[0]] = b[1]
+            self.q3.put([b[0][-1], b[1]])
+    
+    def get_shares(self, name):
+        res = []
+        for i in range(self.n):
+            while name + str(i) not in self.recv:
+                self.readQueue()    
+            res.append(self.F(self.recv[name+str(i)]))                
+        return res
+            
+    def reconstruct_secret(self, name):
+        return ss.rec(self.F, self.get_shares(name))
+    
+    def get_share(self, name):
+        while name not in self.recv:
+            self.readQueue()    
+        return self.F(self.recv[name])                
+    
+    def get_triplets(self):
+        while 'triplets' not in self.recv:
+            self.readQueue()
+        b = self.recv['triplets']
+        res = []
+        for i in b:
+            res.append([self.F(j) for j in i])
+        self.triplets = res
+    
+    def mult_shares(self, a, b):
+        r = self.triplets[self.c]
+        self.c += 1
+        
+        d_local = a - r[0]
+        self.broadcast('d' + str(self.comr), d_local)
+        d_pub = self.reconstruct_secret('d' + str(self.comr))
+        self.comr +=1
+        
+        e_local = b - r[1]
+        self.broadcast('e' + str(self.comr), e_local)
+        e_pub = self.reconstruct_secret('e' + str(self.comr))
+        self.comr+=1
+        
+        return d_pub * e_pub + d_pub*r[1] + e_pub*r[0] + r[2]
+    
+    def legendreComp(self,a,b):
+        r = self.triplets[self.c]
+        self.c+=1
+        t = self.tt
+        g = a - b
+        k = self.mult_shares(t, self.mult_shares(r[0], r[0]))
+        j_loc = self.mult_shares(g, k)
+        self.broadcast('j'+ str(self.comr), j_loc)
+        j_pub = self.reconstruct_secret('j'+str(self.comr))
+        self.comr+=1
+        
+        ex = (self.F.p-1)/2
+        sym = pow(int(str(j_pub)),int(ex), self.F.p)
+        f = sym * t
+        c = self.mult_shares((f+1), self.F(2).inverse())
+        return c
+    
+    def run(self):
+        print('starting party ', self.i)
+        self.get_triplets()
+        self.tt = self.get_share('b')
+        
+## DISTRIBUTE INPUT
+        
+        for j in range(500):
+            data = dd[j]
+#            if j == 0:
+#                data = 0
+#            else:
+#                while True:
+#                    if not self.q2.empty:
+#                        data = self.q2.get()
+#                        break
+#            print('measured pressure:', data)
+            self.distribute_shares(data)
+
+    ## GET INPUT_SHARES  
+            input_shares = self.get_shares('input')
+    # Find minimum using Legendre Comparison:
+            c = self.legendreComp(input_shares[2], input_shares[3])             
+            a = self.mult_shares(1-c,input_shares[2]) + self.mult_shares(c,input_shares[3])            
+            temp = a
+            for i in range(2):
+                    c = self.legendreComp(a, input_shares[i])
+                    a = self.mult_shares(1-c,a)+self.mult_shares(c,input_shares[i])
+            
+            output4 = a
+            
+            output0 = input_shares[0] - output4
+            output1 = input_shares[1] - output4
+            output5 = temp - output4
+            
+            output2 = input_shares[2] - output4 - output5
+            output3 = input_shares[3] - output4 - output5 
+            output = [output0, output1, output2, output3, output4, output5]
+            
+            for i in range(6):
+                sock.TCPclient(party_addr[i][0], party_addr[i][1], ['output' + str(self.i) , int(str(output[i]))])
+            
+            out = int(str(self.reconstruct_secret('output'))) / 100.
+            #sock.UDPclient(server_addr[self.i][0], server_addr[self.i][1], int(str(out)))
+            
+            print('Control ouput: ', out)
+#            time.sleep(1)
+            self.recv = {}
+            self.c = 0
+            self.comr = 0
+        
