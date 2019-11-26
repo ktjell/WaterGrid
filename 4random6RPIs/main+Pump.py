3e55ac2ca8f960cc2f098c565e1323bed3f5f1f1
Katrine <kst@es.aau.dk>
2019-11-26 09:42:32 +0100

    added random data with 6 rpis

---
+++ 4random6RPIs/main+Pump.py
@@ -0,0 +1,187 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 30 11:31:50 2018
+
+@author: kst
+"""
+
+import socket
+import numpy as np
+from threading import Thread
+import FFArithmetic as field
+import shamir_scheme as ss
+import proc
+import TcpSocket5 as sock
+import time
+import queue as que
+from pump import party
+import matplotlib.pyplot as plt
+import os
+from ipcon import ipconfigs as ips
+
+
+
+class commsThread (Thread):
+   stop = False  
+   def __init__(self, threadID, name, server_info,q):
+      Thread.__init__(self)
+      self.q = q
+      self.threadID = threadID
+      self.name = name
+      self.server_info = server_info  # (Tcp_ip, Tcp_port)
+      self.Rx_packet = [] # tuple [[client_ip, client_port], [Rx_data[n]]]
+
+   def run(self):
+#      print("Starting " + self.name)      
+      #Create TCP socket
+      tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+      tcpsock.bind(tuple(self.server_info))
+      #Communication loop - Wait->Receive->Put to queue
+      while not self.stop:
+         Rx_packet = sock.TCPserver(tcpsock)
+#         print("Client info:",Rx_packet[0])
+#         print("Data recv:",Rx_packet[1])
+         if not self.q.full():
+            self.q.put(Rx_packet)
+      print("Exiting " + self.name)
+
+class UDPcommsThread(Thread):
+   def __init__(self, threadID, name, server_info):
+      Thread.__init__(self)
+      self.threadID = threadID
+      self.name = name
+      self.server_info = server_info  # (Tcp_ip, Tcp_port)
+      self.Rx_packet = []  # tuple [[client_ip, client_port], [Rx_data[n]]]
+
+   def run(self):
+      print("Starting " + self.name)
+      #Create UDP socket
+      udpsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      udpsock.bind((self.server_info[0], self.server_info[1]))
+      print('UDP Server Started:', self.server_info[0], self.server_info[1])
+
+      #Communication loop - Wait->Receive->Put to queue
+      while True:
+         Rx_data = sock.UDPserver(udpsock)[0]
+         if not q2.full():
+             q2.put(int(Rx_data*100))
+
+      udpsock.close()
+      print("Exiting " + self.name)
+
+# use ggplot style for more sophisticated visuals
+plt.style.use('ggplot')
+
+class plotter(Thread):
+    def __init__(self,q):
+      Thread.__init__(self)
+#      self.line1 = []
+      self.xdata = np.arange(0,100)
+      self.y0 = np.zeros(100)-1
+      self.y1 = np.zeros(100)-1
+      self.y2 = np.zeros(100)-1
+      self.y3 = np.zeros(100)-1
+      self.q = q
+      
+    def run(self):
+        # this is the call to matplotlib that allows dynamic plotting
+        plt.ion()
+        self.fig = plt.figure(figsize=(13,6))
+        ax = self.fig.add_subplot(111)
+        # create a variable for the line so we can later update it
+        line0, = ax.plot(self.y0,'bo',alpha=0.8)   
+        line1, = ax.plot(self.y1,'ro',alpha=0.8) 
+        line2, = ax.plot(self.y2,'go',alpha=0.8) 
+        line3, = ax.plot(self.y3,'yo',alpha=0.8) 
+        #update plot label/title
+        plt.ylim(0,1)
+        plt.ylabel('data')
+        plt.xlabel('time')
+        plt.title('Received data')
+        plt.show()
+        
+        
+        while True:
+            if not self.q.empty():
+                b = self.q.get()
+                if b[0] == '0':
+                    self.y0 = self.ploting(line0, self.y0, b[1])
+                if b[0] == '1':
+                    self.y1 = self.ploting(line1, self.y1, b[1])                                                                                                                                                                                              
+                if b[0] == '2':
+                    self.y2 = self.ploting(line2, self.y2, b[1])
+                if b[0] == '3':
+                    self.y3 = self.ploting(line3, self.y3, b[1])
+            
+    def ploting(self, line, ydata, y):
+        if not isinstance(y, list):
+            yl = ydata[:-1]
+            ydata = np.insert(yl,0, y/float(m))
+#            if isinstance(y[0], list):
+#                return
+#            else:
+#                yl = self.ydata[len(y):]
+#                self.ydata = np.concatenate((yl, np.array(y)/float(m)))
+        else:
+           return ydata
+            
+           
+        
+        # after the figure, axis, and line are created, we only need to update the y-data
+        line.set_ydata(ydata)
+        # adjust limits if new data goes beyond bounds
+#        if np.min(self.ydata)<=self.line1.axes.get_ylim()[0] or np.max(self.ydata)>=self.line1.axes.get_ylim()[1]:
+#            plt.ylim([np.min(self.ydata)-np.std(self.ydata),np.max(self.ydata)+np.std(self.ydata)])
+        # this pauses the data so the figure/axis can catch up - the amount of pause can be altered above
+#        plt.pause(0.1)
+        self.fig.canvas.draw()
+        return ydata
+        
+m = 7979490791
+F = field.GF(m)            
+n = 4
+t = 1
+x = 5
+
+ipv4 = os.popen('ip addr show eth0').read().split("inet ")[1].split("/")[0]
+pnr = ips.party_addr.index([ipv4, ips.port])
+q = que.Queue()
+q2 = que.LifoQueue()
+q3 = que.Queue()
+
+#Initialization..
+#TCP_IP = '192.168.100.246'
+#TCP_PORT = 62
+UDP_PORT2 = 3000
+server_info = ips.party_addr[pnr]#(TCP_IP, TCP_PORT)
+server2_info = (ipv4, UDP_PORT2)
+
+# Create new threads..
+t1_comms = commsThread(1, "Communication Thread", server_info,q)
+#t2_commsSimulink = UDPcommsThread(2, "t2_commsSimulink", server2_info)
+ploting = plotter(q3)
+ploting.start()
+p = party(F,int(x),n,t,pnr, q, q2, q3, ips.party_addr, ips.server_addr)
+
+# Start new Threads
+#t2_commsSimulink.start()
+t1_comms.start()
+
+for i in ips.party_addr:
+    while True:
+        try:
+            sock.TCPclient(i[0], i[1], ['flag', 1])
+            break
+        except:
+            time.sleep(1)
+            continue
+
+p.start()
+
+
+
+
+
+
+
---
+++ 4random6RPIs/pump.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 11 09:01:04 2018
+
+@author: kst
+"""
+
+
+import numpy as np
+from threading import Thread
+import shamir_scheme as ss
+import TcpSocket5 as sock
+import queue as que
+
+ite = 1800
+
+class party(Thread):
+    def __init__(self, F, x, n, t, i, q, q2,q3, paddr, saddr):
+        Thread.__init__(self)
+        self.c = 0
+        self.comr = 0
+        self.recv = {}
+        self.F = F
+        self.x = x
+        self.n = n
+        self.t = t
+        self.i = i
+        self.q = q
+        self.q2 = q2
+        self.q3 = q3
+        self.party_addr = paddr
+        self.server_addr = saddr
+        
+    def distribute_shares(self, sec):
+        shares = ss.share(self.F, sec, self.t, self.n)
+        for i in range(self.n):
+            sock.TCPclient(self.party_addr[i][0], self.party_addr[i][1], ['input' + str(self.i) , int(str(shares[i]))])
+        
+    def broadcast(self, name, s):
+        for i in range(self.n):
+            sock.TCPclient(self.party_addr[i][0], self.party_addr[i][1], [name + str(self.i) , int(str(s))])
+                    
+    def readQueue(self):
+        while not self.q.empty():
+            b = self.q.get()[1]
+            self.recv[b[0]] = b[1]
+            self.q3.put([b[0][-1], b[1]])
+    
+    def get_shares(self, name):
+        res = []
+        for i in range(self.n):
+            while name + str(i) not in self.recv:
+                self.readQueue()    
+            res.append(self.F(self.recv[name+str(i)]))
+            del self.recv[name + str(i)]              
+        return res
+            
+    def reconstruct_secret(self, name):
+        return ss.rec(self.F, self.get_shares(name))
+    
+    def get_share(self, name):
+        while name not in self.recv:
+            self.readQueue()
+        a = self.F(self.recv[name])
+        del self.recv[name]
+        return a
+       
+    def run(self):
+        for j in range(ite):          
+            out = int(str(self.reconstruct_secret('output'))) / 100.
+#            sock.UDPclient(self.server_addr[self.i][0], self.server_addr[self.i][1], out)
+            
+            print('Control ouput: ', out)
+#            time.sleep(1)
+#            self.recv = {}
+            self.c = 0
+            self.comr = 0
+        
