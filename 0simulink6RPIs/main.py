b74412a72c0b84d248feee95f1f6b472ab7444b1
Katrine <kst@es.aau.dk>
2020-09-22 11:20:25 +0200

    changed a bit in main

--- 0simulink6RPIs/main.py
+++ 0simulink6RPIs/main.py
@@ -79,7 +79,10 @@ class plotter(Thread):
     def __init__(self,q1,q2):
       Thread.__init__(self)
 #      self.line1 = []
-      self.xdata = np.arange(0,100)
+      self.x1 = np.arange(-99,1)
+      self.x2 = np.arange(-99,1)
+      self.x3 = np.arange(-99,1)
+      self.x4 = np.arange(-99,1)
       self.y0 = np.zeros(100)-1
       self.y1 = np.zeros(100)-1
       self.y2 = np.zeros(100)-1
@@ -94,16 +97,22 @@ class plotter(Thread):
         # this is the call to matplotlib that allows dynamic plotting
         plt.ion()
         self.fig1 = plt.figure(figsize=(13,6))
-        self.fig2 = plt.figure(figsize=(13,6))
-        ax = self.fig1.add_subplot(111)
+        self.ax = self.fig1.add_subplot(111)
         
+        # create a variable for the line so we can later update it
+        line0, = self.ax.plot(self.x1, self.y0,'bo',alpha=0.8)   
+        line1, = self.ax.plot(self.x2, self.y1,'ro',alpha=0.8) 
+        line2, = self.ax.plot(self.x3, self.y2,'go',alpha=0.8) 
+        line3, = self.ax.plot(self.x4, self.y3,'yo',alpha=0.8) 
+        self.ax.set_ylim(0,1)
+        self.ax.set_ylabel('data')
+        self.ax.set_xlabel('time')
+        self.ax.set_title('Received data')
+        
+        
+        self.fig2 = plt.figure(figsize=(13,6))
         ax1 = self.fig2.add_subplot(211)
         ax2 = self.fig2.add_subplot(212)
-        # create a variable for the line so we can later update it
-        line0, = ax.plot(self.y0,'bo',alpha=0.8)   
-        line1, = ax.plot(self.y1,'ro',alpha=0.8) 
-        line2, = ax.plot(self.y2,'go',alpha=0.8) 
-        line3, = ax.plot(self.y3,'yo',alpha=0.8) 
         #update plot label/title
         ax1.set_ylim(0,1)
         ax1.set_ylabel('data')
@@ -125,41 +134,44 @@ class plotter(Thread):
                 b2 = self.q2.get()
                 self.ploting2(ax2,lineA, b2[1])
                 
-#            if not self.q1.empty():
-#                b = self.q1.get()
-#                if b[0] == '0':
-#                    self.y0 = self.ploting(line0, self.y0, b[1])
-#                if b[0] == '1':
-#                    self.y1 = self.ploting(line1, self.y1, b[1])                                                                                                                                                                                              
-#                if b[0] == '2':
-#                    self.y2 = self.ploting(line2, self.y2, b[1])
-#                if b[0] == '3':
-#                    self.y3 = self.ploting(line3, self.y3, b[1])
+            if not self.q1.empty():
+                b = self.q1.get()
+                if b[0] == '0':
+                     self.x1, self.y0 = self.ploting(line0, self.x1, self.y0, b[1])
+                if b[0] == '1':
+                     self.x2, self.y1 = self.ploting(line1, self.x2, self.y1, b[1])                                                                                                                                                                                              
+                if b[0] == '2':
+                     self.x3, self.y2 = self.ploting(line2, self.x3, self.y2, b[1])
+                if b[0] == '3':
+                    self.x4, self.y3 = self.ploting(line3, self.x4, self.y3, b[1])
                 
             
-    def ploting(self, line, ydata, y):
+    def ploting(self, line, xdata, ydata, y):
         if not isinstance(y, list):   #Hvis y IKKE er en list gør:
-            yl = ydata[:-1]
-            ydata = np.insert(yl,0, y/float(m))
+            ydata = np.append(ydata[1:], y/float(m))
+            xdata = xdata + 1
+
 #            if isinstance(y[0], list):
 #                return
 #            else:
 #                yl = self.ydata[len(y):]
 #                self.ydata = np.concatenate((yl, np.array(y)/float(m)))
         else:
-           return ydata
+           return xdata, ydata
             
            
         
         # after the figure, axis, and line are created, we only need to update the y-data
+        line.set_xdata(xdata)
         line.set_ydata(ydata)
+        self.ax.set_xlim(max(0, min( min(self.x1), min(self.x2),min(self.x3), min(self.x4) )), max( max(self.x1), max(self.x2), max(self.x3), max(self.x4)) +1)
         # adjust limits if new data goes beyond bounds
 #        if np.min(self.ydata)<=self.line1.axes.get_ylim()[0] or np.max(self.ydata)>=self.line1.axes.get_ylim()[1]:
 #            plt.ylim([np.min(self.ydata)-np.std(self.ydata),np.max(self.ydata)+np.std(self.ydata)])
         # this pauses the data so the figure/axis can catch up - the amount of pause can be altered above
 #        plt.pause(0.1)
         self.fig1.canvas.draw()
-        return ydata
+        return xdata, ydata
     
     def ploting2(self, ax2, line, y):
 
